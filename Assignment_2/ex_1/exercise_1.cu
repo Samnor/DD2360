ac3678b16e63e4fdab1588d249a89f75c5d250b2
Samuel Norling <norling.samuel@gmail.com>
2020-11-10 18:12:08 +0100

    Update exercise_1.cu

--- Assignment_2/ex_1/exercise_1.cu
+++ Assignment_2/ex_1/exercise_1.cu
@@ -8,10 +8,6 @@ __global__ void cuda_hello(){
 }
 
 int main() {
-    /*
-    Set the kernel to run with 256 threads in 1 single thread block,
-    following a 1D distribution.
-    */
     cuda_hello<<<N/TPB,TPB>>>();
     cudaDeviceSynchronize();
     return 0;
